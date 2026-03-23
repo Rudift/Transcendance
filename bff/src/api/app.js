b12a9dae1f68fdfb5f4ed4415cd4e66373dd8f56
Valentin Deliere <vdeliere@f6r7s9.clusters.42paris.fr>
2026-03-23 16:34:03 +0100

    feat: new metric: nb request by service

--- bff/src/api/app.js
+++ bff/src/api/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
-require('../metrics/registry.js');
+const { promClient, register } = require('../metrics/registry.js');
 
 const app = express();
 app.use(express.json());
+
+const httpsRequestsTotal = new promClient.Counter({
+	name: 'https_requests_total',
+	help : 'Total HTTPS requests',
+	labelNames: ['method', 'route', 'status_code'],
+	registers: [register],
+});
+
+app.use((req, res, next) => {
+	res.on('finish', () => {
+		const route = req.route?.path ?? req.path;
+		httpsRequestsTotal.inc({
+			method: req.method,
+			route,
+			status_code: String(res.statusCode),
+		});
+	});
+	next();
+});
 
 app.get('/health', (req, res) => {
 	res.json({ ok: true });
